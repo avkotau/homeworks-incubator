b5019ffd924947fcdbd83aa84ee059ebaf6fd45f
avkotau <kotov.alexander1@mail.ru>
2023-06-11 19:48:17 +0200

    feat: add sort name, filter age, styled component

--- src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
+++ src/s2-homeworks/hw15/common/c9-SuperPagination/SuperPagination.tsx
@@ -48,9 +48,9 @@ const SuperPagination: React.FC<SuperPaginationPropsType> = (
                 id={id + '-pagination-select'}
                 value={itemsCountForPage}
                 options={[
-                    {id: 4, value: 4},
-                    {id: 7, value: 7},
-                    {id: 10, value: 10},
+                    {id: 4, value:'4'}, // changed 4 to '4' support it incubator 01.06.2023
+                    {id: 7, value: '7'},
+                    {id: 10, value: '10'},
                 ]}
                 onChange={onChangeSelect}
             />
